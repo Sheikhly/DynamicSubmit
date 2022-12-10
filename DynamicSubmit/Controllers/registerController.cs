3315e7a772b3f5e0735d38f04988ff84dacbcc3b
HP <HP@DESKTOP-7T8T92K>
2022-12-10 17:28:46 +0330

    Add project files.

---
+++ DynamicSubmit/Controllers/registerController.cs
@@ -0,0 +1,29 @@
+﻿using DynamicSubmit.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DynamicSubmit.Controllers
+{
+    public class registerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+
+
+
+        [HttpPost]
+        public ActionResult Index(Info model)
+        {
+            
+            return Content("R");
+        }
+
+       
+
+
+    }
+}
---
+++ DynamicSubmit/Models/Info.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
+namespace DynamicSubmit.Models
+{
+    public class Info
+    {    
+        [Display(Name = "Enter your first name:")]
+        public string name { get; set; }
+
+        [Display(Name = "Enter your last name:")]
+        public string family  { get; set; }
+
+        [Display(Name = "Description:")]
+        public string description { get; set; }
+
+        [Display(Name = "Date of Your Birth:")]
+        public string DOF { get; set; }
+        
+
+    }
+}
